4e1b9b03115572cb21baf907dfd9f0df924e189b
Ricardo Rivera <glassboxwebmaster@gmail.com>
2021-04-09 19:00:17 -0700

    initial commit

---
+++ script/script.js
@@ -0,0 +1,52 @@
+var video = document.getElementById("logo-video");
+video.addEventListener("canplay", function () {
+  setTimeout(function () {
+    video.play();
+  }, 2000);
+});
+
+var player = document.getElementById("player");
+let progress = document.getElementById("progress");
+let playbtn = document.getElementById("playbtn");
+
+var playpause = function () {
+  if (player.paused) {
+    player.play();
+  } else {
+    player.pause();
+  }
+};
+
+playbtn.addEventListener("click", playpause);
+
+player.onplay = function () {
+  playbtn.classList.remove("fa-play");
+  playbtn.classList.add("fa-pause");
+};
+
+player.onpause = function () {
+  playbtn.classList.add("fa-play");
+  playbtn.classList.remove("fa-pause");
+};
+
+player.ontimeupdate = function () {
+  let ct = player.currentTime;
+  current.innerHTML = timeFormat(ct);
+  /* Progress */
+  let duration = player.duration;
+  prog = Math.floor((ct * 100) / duration);
+  progress.style.setProperty("--progress", prog + "%");
+};
+
+function timeFormat(ct) {
+  minutes = Math.floor(ct / 60);
+  seconds = Math.floor(ct % 60);
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  return minutes + ":" + seconds;
+}
+
+/* Song Array */
